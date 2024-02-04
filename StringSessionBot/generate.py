e93846418dbd0ec7d9c9cacf88e19b21224b74d3
mrlord856 <151881197+mrlord856@users.noreply.github.com>
2024-02-04 06:08:53 +0200

    Add files via upload

---
+++ StringSessionBot/generate.py
@@ -0,0 +1,132 @@
+from asyncio.exceptions import TimeoutError
+from Data import Data
+from pyrogram import Client, filters
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import (
+    ApiIdInvalid,
+    PhoneNumberInvalid,
+    PhoneCodeInvalid,
+    PhoneCodeExpired,
+    SessionPasswordNeeded,
+    PasswordHashInvalid
+)
+from telethon.errors import (
+    ApiIdInvalidError,
+    PhoneNumberInvalidError,
+    PhoneCodeInvalidError,
+    PhoneCodeExpiredError,
+    SessionPasswordNeededError,
+    PasswordHashInvalidError
+)
+
+
+@Client.on_message(filters.private & ~filters.forwarded & filters.command('generate'))
+async def main(_, msg):
+    await msg.reply(
+        "¤¦ يرجى تحديد نوع الجلسة المراد استخراجها",
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton("¤¦ كود بـايروجرام", callback_data="pyrogram"),
+            InlineKeyboardButton("¤¦ كود تيرمكس", callback_data="telethon")
+        ]])
+    )
+
+
+async def generate_session(bot, msg, telethon=False):
+    await msg.reply("Starting {} Session Generation...".format("Telethon" if telethon else "Pyrogram"))
+    user_id = msg.chat.id
+    api_id_msg = await bot.ask(user_id, '🔰حسنا يرجى ارسال ال `API_ID`🔰', filters=filters.text)
+    if await cancelled(api_id_msg):
+        return
+    try:
+        api_id = int(api_id_msg.text)
+    except ValueError:
+        await api_id_msg.reply('Not a valid API_ID (which must be an integer). Please start generating session again.', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return
+    api_hash_msg = await bot.ask(user_id, '🔰حسنا يرجى ارسال ال `API_HASH`🔰', filters=filters.text)
+    if await cancelled(api_id_msg):
+        return
+    api_hash = api_hash_msg.text
+    phone_number_msg = await bot.ask(user_id, '- الان يرجى ارسال `PHONE_NUMBER` مع رمز الدوله . \nمثال : `+201153987165`', filters=filters.text)
+    if await cancelled(api_id_msg):
+        return
+    phone_number = phone_number_msg.text
+    await msg.reply("Sending OTP...")
+    if telethon:
+        client = TelegramClient(StringSession(), api_id, api_hash)
+    else:
+        client = Client(":memory:", api_id, api_hash)
+    await client.connect()
+    try:
+        if telethon:
+            code = await client.send_code_request(phone_number)
+        else:
+            code = await client.send_code(phone_number)
+    except (ApiIdInvalid, ApiIdInvalidError):
+        await msg.reply('`API_ID` and `API_HASH` combination - أدخلت كلمة مرور غير صالحة. يرجى البدء في إنشاء الجلسة مرة أخرى.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return
+    except (PhoneNumberInvalid, PhoneNumberInvalidError):
+        await msg.reply('`PHONE_NUMBER` - أدخلت كلمة مرور غير صالحة. يرجى البدء في إنشاء الجلسة مرة أخرى.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return
+    try:
+        phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.", filters=filters.text, timeout=600)
+        if await cancelled(api_id_msg):
+            return
+    except TimeoutError:
+        await msg.reply('بلغ الحد الزمني 10 دقائق. يرجى البدء في إنشاء الجلسة مرة أخرى.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return
+    phone_code = phone_code_msg.text.replace(" ", "")
+    try:
+        if telethon:
+            await client.sign_in(phone_number, phone_code, password=None)
+        else:
+            await client.sign_in(phone_number, code.phone_code_hash, phone_code)
+    except (PhoneCodeInvalid, PhoneCodeInvalidError):
+        await msg.reply('OTP - أدخلت كلمة مرور غير صالحة. يرجى البدء في إنشاء الجلسة مرة أخرى.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return
+    except (PhoneCodeExpired, PhoneCodeExpiredError):
+        await msg.reply('OTP is expired. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return
+    except (SessionPasswordNeeded, SessionPasswordNeededError):
+        try:
+            two_step_msg = await bot.ask(user_id, 'Your account has enabled two-step verification. Please provide the password.', filters=filters.text, timeout=300)
+        except TimeoutError:
+            await msg.reply('Time limit reached of 5 minutes. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+            return
+        try:
+            password = two_step_msg.text
+            if telethon:
+                await client.sign_in(password=password)
+            else:
+                await client.check_password(password=password)
+            if await cancelled(api_id_msg):
+                return
+        except (PasswordHashInvalid, PasswordHashInvalidError):
+            await two_step_msg.reply('Invalid Password Provided. Please start generating session again.', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+            return
+    if telethon:
+        string_session = client.session.save()
+    else:
+        string_session = await client.export_session_string()
+    text = "**{} STRING SESSION** \n\n`{}` \n\من قبل  @M_R_ZC".format("TELETHON" if telethon else "PYROGRAM", string_session)
+    try:
+        await client.send_message("me", text)
+    except KeyError:
+        pass
+    await client.disconnect()
+    await phone_code_msg.reply("Successfully generated {} string session. \n\nPlease check your saved messages! \n\nBy @M_R_ZC".format("telethon" if telethon else "pyrogram"))
+
+
+async def cancelled(msg):
+    if "/cancel" in msg.text:
+        await msg.reply("Cancelled the Process!", quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return True
+    elif "/restart" in msg.text:
+        await msg.reply("Restarted the Bot!", quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        return True
+    elif msg.text.startswith("/"):  # Bot Commands
+        await msg.reply("!ألغيت عملية التوليد", quote=True)
+        return True
+    else:
+        return False
